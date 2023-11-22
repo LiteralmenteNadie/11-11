5b5b96ac3bb58ed0e48c71f85d5e29d1e5180e60
coordis 13a18 <cordis@puerta18.corp.ar>
2023-11-22 11:17:44 -0300

    Modificada la información de acceso a la DB

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -8,17 +8,19 @@ module.exports = {
     //"port": 3306
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": "asd",
+    "password": "Programacion9-12",
+    "database": "asd_pan_db",
+    "host": "mysql-asd.alwaysdata.net", // 127.0.0.1 == localhost
+    "dialect": "mysql",
+    //"port": 3306
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": "asd",
+    "password": "Programacion9-12",
+    "database": "asd_pan_db",
+    "host": "mysql-asd.alwaysdata.net", // 127.0.0.1 == localhost
+    "dialect": "mysql",
+    //"port": 3306
   }
 }
